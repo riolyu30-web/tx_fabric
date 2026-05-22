a9d01ff76d4330e89c8fd9743750213d39c88b22
雷晓锋 <lxfleixiaofeng@163.com>
2026-05-22 14:48:13 +0800

    “i18n3”

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'tx_fabric',
+    script: 'npm',
+    args: 'start',
+    cwd: '/var/www/tx_fabric',
+    instances: 'max',
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 9090  // 修改为9090端口
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }]
+};
